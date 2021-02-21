1d7044f1dd33c210adc1ab2d268c79670113ac40
zoe <1670828846@qq.com>
2021-02-21 15:27:18 +0800

    tab问题修复

--- src/myLib/service/display/ili9341/tftlcd.h
+++ src/myLib/service/display/ili9341/tftlcd.h
@@ -7,7 +7,6 @@
 #include "myLib/drv/spi/hardware_spi.h"
 
 
-<<<<<<< HEAD
 #define WHITE   0xFFFF
 #define BLACK   0x0000
 #define BLUE    0x001F
@@ -22,22 +21,6 @@
 #define BROWN   0XBC40
 #define BRRED   0XFC07
 #define GRAY    0X8430
-=======
-#define WHITE         	 0xFFFF
-#define BLACK         	 0x0000
-#define BLUE         	 0x001F
-#define BRED             0XF81F
-#define GRED 			 0XFFE0
-#define GBLUE			 0X07FF
-#define RED           	 0xF800
-#define MAGENTA       	 0xF81F
-#define GREEN         	 0x07E0
-#define CYAN          	 0x7FFF
-#define YELLOW        	 0xFFE0
-#define BROWN 			 0XBC40
-#define BRRED 			 0XFC07
-#define GRAY  			 0X8430
->>>>>>> c693649d3dec1b5e870cd48ce7c08c6b3d3d31e5
 
 
 void LCD_WriteIndex(uint8_t command);
